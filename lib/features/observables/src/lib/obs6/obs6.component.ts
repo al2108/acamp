54e385ec27f5e71c2ce60fabf63f9c2c71473fda
alewark <Andreas.Lewark@t-systems.com>
2023-04-12 17:40:45 +0200

    .

--- lib/features/observables/src/lib/obs6/obs6.component.ts
+++ lib/features/observables/src/lib/obs6/obs6.component.ts
@@ -1,20 +1,37 @@
 import {CommonModule} from '@angular/common';
 import {Component, OnInit, inject} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, map, throttleTime} from 'rxjs';
 import {Obs6Service, TemperatureMeasurement} from './obs6.service';
+
+interface ExtendedTemperatureMeasurement extends TemperatureMeasurement {
+  sum: number;
+  count: number;
+}
 
 @Component({
   selector: 'acamp-obs6',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './obs6.component.html',
-  styleUrls: ['./obs6.component.scss'],
+  styleUrls: ['./obs6.component.scss']
 })
 export class Obs6Component implements OnInit {
-  public measuredValues!: Observable<TemperatureMeasurement>;
+  public measuredValues1!: Observable<TemperatureMeasurement>;
+  public measuredValues2!: Observable<ExtendedTemperatureMeasurement>;
+
   private measureService: Obs6Service = inject(Obs6Service);
 
   public ngOnInit(): void {
-    this.measuredValues = this.measureService.getTemperatureMeasurements();
+    let measuredTempSum = 0;
+    let measuredTempCount = 0;
+    this.measuredValues1 = this.measureService.getTemperatureMeasurements();
+    this.measuredValues2 = this.measureService.getTemperatureMeasurements().pipe(
+      map(value => {
+        measuredTempSum += value.temperature;
+        measuredTempCount++;
+        return {...value, sum: measuredTempSum, count: measuredTempCount};
+      }),
+      throttleTime(5000)
+    );
   }
 }
