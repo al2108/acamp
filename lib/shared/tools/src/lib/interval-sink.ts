7d262cc09f18c5a44840d735b8299bc9caa36fc2
wd4711 <106246831+wd4711@users.noreply.github.com>
2023-04-14 10:42:52 +0200

    .

---
+++ lib/shared/tools/src/lib/interval-sink.ts
@@ -0,0 +1,19 @@
+type Timer = NodeJS.Timer;
+type TimerList = Array<Timer>;
+
+export class IntervalSink {
+  protected intervals: TimerList = [];
+
+  public add(...intervals: TimerList): void {
+    this.intervals = [...this.intervals, ...intervals];
+  }
+
+  public set sink(interval: Timer) {
+    this.intervals.push(interval);
+  }
+
+  public unset(): void {
+    this.intervals.forEach(interval => clearInterval(interval));
+    this.intervals = [];
+  }
+}
