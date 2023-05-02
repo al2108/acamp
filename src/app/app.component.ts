4f0c93e15a56148a56705596255a98a19451abcc
al <alewark@gmail.com>
2023-05-02 15:44:39 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {LibFeaturesAboutModule} from '@acamp/lib/features/about';
 import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {LibFeaturesAboutModule} from '@acamp/lib/features/about';
 
 @Component({
   standalone: true,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,28 @@
-import {AboutComponent} from '@acamp/lib/features/about';
 import {decoratorRoutes} from '@acamp/lib/features/decorators';
-import {dialogRoutes} from '@acamp/lib/features/dialogs';
 import {directiveRoutes} from '@acamp/lib/features/directives';
-import {HomeComponent} from '@acamp/lib/features/home';
 import {pipeRoutes} from '@acamp/lib/features/pipes';
 import {stateRoutes} from '@acamp/lib/features/sates';
 import {templateRoutes} from '@acamp/lib/features/templates';
 import {Route} from '@angular/router';
+import {AboutComponent} from '@acamp/lib/features/about';
+import {HomeComponent} from '@acamp/lib/features/home';
 
 export const routes: Array<Route> = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  // {path: 'home-lazy', loadComponent: () => import('@acamp/lib/features/home').then(module => module.HomeComponent)},
+  // {path: 'home', loadComponent: () => import('@acamp/lib/features/home').then(component => component.HomeComponent)},
   // {
-  //   path: 'about-lazy',
-  //   loadChildren: () => import('@acamp/lib/features/about').then(module => module.LibFeaturesAboutModule),
+  //   path: 'about',
+  //   loadChildren: () => import('@acamp/lib/features/about').then(module => module.LibFeaturesAboutModule)
   // },
   {
     path: 'observables',
-    loadChildren: () => import('@acamp/lib/features/observables').then(m => m.observableRoutes)
+    loadChildren: () => import('@acamp/lib/features/observables').then(routes => routes.observableRoutes)
   },
   {
     path: 'dialogs',
-    children: dialogRoutes
+    loadChildren: () => import('@acamp/lib/features/dialogs').then(routes => routes.dialogRoutes)
   },
   {
     path: 'templates',
